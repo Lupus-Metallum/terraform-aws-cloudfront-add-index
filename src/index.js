44a12b83078b11614eb2f05db36f2aab1d908df3
Nick Adams <contact@nickadams.guru>
2021-10-21 22:17:08 -0700

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,20 @@
+'use strict';
+
+exports.handler = (event, context, callback) => {
+    /*
+     * Append index.html or /index.html if needed
+     */
+    var request = event.request;
+    var uri = request.uri;
+
+    // Check whether the URI is missing a file name.
+    if (uri.endsWith('/')) {
+        request.uri += 'index.html';
+    }
+    // Check whether the URI is missing a file extension.
+    else if (!uri.includes('.')) {
+        request.uri += '/index.html';
+    }
+
+    return request;
+};
